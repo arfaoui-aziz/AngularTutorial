936b8db73a07a240b623803d62b613d7055c3ab9
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2020-12-03 09:37:49 +0100

    Angular Project V0

--- AngularProject/e2e/src/app.e2e-spec.ts
+++ AngularProject/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('CourseProject app is running!');
+    expect(await page.getTitleText()).toEqual('AngularProject app is running!');
   });
 
   afterEach(async () => {
--- AngularProject/karma.conf.js
+++ AngularProject/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/CourseProject'),
+      dir: require('path').join(__dirname, './coverage/AngularProject'),
       subdir: '.',
       reporters: [
         { type: 'html' },
